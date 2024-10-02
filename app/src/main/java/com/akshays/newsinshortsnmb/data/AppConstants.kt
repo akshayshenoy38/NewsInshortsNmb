4d6d9753d98ac6fd2bca30375476053a2fa876d1
Akshay Shenoy <30587680+akshayshenoy38@users.noreply.github.com>
2024-10-02 21:50:45 +0530

    Update AppConstants.kt

--- app/src/main/java/com/akshays/newsinshortsnmb/data/AppConstants.kt
+++ app/src/main/java/com/akshays/newsinshortsnmb/data/AppConstants.kt
@@ -3,6 +3,6 @@ package com.akshays.newsinshortsnmb.data
 object AppConstants {
 
     const val APP_BASE_URL = "https://newsapi.org/"
-    const val API_KEY = "f32810135f364627b1fedc321ab5a352"
+    const val API_KEY = "ENTER Tour Own Key Here for NewsApi.org"
     const val COUNTRY = "us"
 }
